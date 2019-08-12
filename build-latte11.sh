c84755185450cb613d9349c56b4bcebccf709dd4
Ajit Pendse <ajit.pendse@elastic.run>
2019-08-12 13:01:54 +0000

    Fix: syntax error in build script.

--- build-latte11.sh
+++ build-latte11.sh
@@ -4,5 +4,5 @@ TAG=$1
 #Temporarily uses docker branch from development project - not from deployment project.
 echo "Kafka Config : '${KAFKA_CONFIG}'"
 docker build -f Dockerfile --build-arg CUR_DATE=$(date +%Y-%m-%d:%H:%M:%S) --build-arg KAFKA_CONFIG='${KAFKA_CONFIG}' \
-  --build-arg LATTE_BRANCH=docker --build-arg GIT_LATTE_URL=engg.elasticrun.in/with-run/latte.git
+  --build-arg LATTE_BRANCH=docker --build-arg GIT_LATTE_URL=engg.elasticrun.in/with-run/latte.git \
   -t dock.elasticrun.in/er-latte11-base:${TAG} .
