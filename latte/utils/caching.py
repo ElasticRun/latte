890689908be6c6847a934e496e84fac45437320b
Himanshu Mishra <himanshu.mishra@elastic.run>
2019-09-11 03:06:04 +0530

    lockless caching strategy

--- latte/utils/caching.py
+++ latte/utils/caching.py
@@ -1,7 +1,7 @@
 import frappe
 import inspect
-from latte.utils.locking import lock
 import pickle
+import gevent
 
 def cache_me_if_you_can(expiry=20):
     def decorator(fn):
@@ -10,18 +10,33 @@ def cache_me_if_you_can(expiry=20):
             for idx, arg in enumerate(args):
                 kwargs[arg_names[idx]] = arg
             param_slug = frappe.as_json(kwargs, indent=None).replace('\"', '')
+
             method_name = f'{fn.__module__}.{fn.__name__}'
             slug = f"{method_name}**{param_slug}"
+
             cache = frappe.cache()
             cached_value = cache.get_value(slug)
             if cached_value:
+                if cached_value == '__PENDING__':
+                    gevent.sleep(1)
+                    return decorated(**kwargs)
                 print('@@@@@@@@@@@@@@@@@@From Cache')
                 return pickle.loads(cached_value)
-            print('@@@@@@@@@@@@@@Cache miss', f'{fn.__module__}.{fn.__name__}')
-            lock(slug)
-            response = fn(**kwargs)
+
+            exclusive = cache.set(slug, '__PENDING__', nx=True)
+            print('@@@@@@@@@@@@@@Cache miss', f'{fn.__module__}.{fn.__name__}', exclusive)
+            if not exclusive:
+                gevent.sleep(1)
+                return decorated(**kwargs)
+            try:
+                response = fn(**kwargs)
+            except:
+                raise
+            finally:
+                cache.delete(slug)
             cached_value = pickle.dumps(response)
             cache.set_value(slug, cached_value, expires_in_sec=expiry)
             return response
+
         return decorated
     return decorator
--- start_guni.py
+++ start_guni.py
@@ -23,8 +23,8 @@ SIMPLE_FORMAT = '%(m)s %(U)s %(q)s := %(s)s | %(D)s | %(B)s'
 sys.argv += [
     '-t', '120',
     'latte.app:application',
-    '--worker-connections', '4',
-    '-w', '1',
+    '--worker-connections', '40',
+    '-w', '2',
     '-k', 'gevent',
     '--access-logfile', '-',
     '--access-logformat', SIMPLE_FORMAT,
